d2f2967f9682185d89d031f79d9290afac37120b
julietruan <122883465+julietruan@users.noreply.github.com>
2023-11-02 12:51:28 +1100

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_fruityvice_data(this_fruit_choice):
   # import requests
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
   # flattern json file
-  fruityvice_normalized = pandas.json_normalize(fruityvice_reponsde.json())
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
 #New section to display fruityvice api response
